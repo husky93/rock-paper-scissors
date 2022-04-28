8ec63c1441e13bf3e27aeee460d4fa0d08d4faed
Maciej Sroka <sroka.maciej93@gmail.com>
2022-04-28 20:27:26 +0200

    Add cancel exception handling

--- script.js
+++ script.js
@@ -34,15 +34,20 @@ function game() {
     let computerScore = 0;
     let playerScore = 0;
     for(let i = 0; i < 5; i++) {
-        const playerChoice = prompt('Choose! Rock, Paper or Scissors!');
-        playerChoice = playerChoice.toLowerCase();
-        if(playerChoice !== 'rock' || playerChoice !== 'paper' || playerChoice !== 'scissors') {
-            i--;
+        let playerChoice = prompt('Choose! Rock, Paper or Scissors!');
+        playerChoice = playerChoice === null ? playerChoice : playerChoice.toLowerCase();
+        if(playerChoice === null) {
+            alert('You cancelled the game!');
             break;
+        } else if(playerChoice !== 'rock' || playerChoice !== 'paper' || playerChoice !== 'scissors') {
+            alert('You can choose only rock, paper or scissors!');
+            i--;
+            continue;
         }
-
         console.log(`Score is:
         Player: ${playerScore} - Computer: ${computerScore}`);
     }
     computerScore > playerScore ? console.log('Computer wins!') ? playerScore > computerScore: console.log('Player wins!') : console.log('Draw!');
 }
+
+game();
