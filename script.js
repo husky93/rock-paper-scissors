5c2569e8831351aca30c670fc794f4cec06a6e8e
Maciej Sroka <sroka.maciej93@gmail.com>
2022-04-28 19:59:22 +0200

    Create function playRound that determines a winner

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-console.log(computerPlay());
+console.log(playRound('rock', computerPlay()))
 
 function computerPlay() {
     const choice = ['rock', 'paper', 'scissors'];
     const index = Math.floor(Math.random() * 3);    //Get random number between 0-2
-    console.log(index);
     return choice[index];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch (true) {
+        case(playerSelection === 'rock' && computerSelection === 'paper'):
+            return 'You lose! Paper beats rock.';
+       
+        case(playerSelection === 'rock' && computerSelection === 'scissors'):
+            return 'You win! Rock beats scissors.';
+
+        case(playerSelection === 'paper' && computerSelection === 'rock'):
+            return 'You win! Paper beats rock.';
+
+        case(playerSelection === 'paper' && computerSelection === 'scissors'):
+            return 'You lose! Scissors beat paper.';
+
+        case(playerSelection === 'scissors' && computerSelection === 'rock'):
+            return 'You lose! Rock beats scissors.';
+
+        case(playerSelection === 'scissors' && computerSelection === 'paper'):
+            return 'You win! Scissors beat paper.';
+
+        default:
+            return `Draw! You both chose ${playerSelection}.`;
+    }
 }
